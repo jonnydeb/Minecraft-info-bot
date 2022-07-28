ca0f676b075f66eee271e59ab6e9e8f8e171768f
jonnydeb <110197170+jonnydeb@users.noreply.github.com>
2022-07-28 21:56:00 +0200

    Create modpack.js

---
+++ commands/modpack.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'modpack',
+    description: 'the link for the modpack',
+    execute(message) {
+        message.channel.send('You can install the modpack at:https://www.curseforge.com/minecraft/modpacks/mekanism-automation');
+    }
+}
